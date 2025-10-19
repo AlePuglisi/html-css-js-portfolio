038a35016f04e88cc94ee5e7e78143e540eb18cb
Alessandro Puglisi <puglisialessandro27@gmail.com>
2025-10-19 17:13:25 +0200

    chore: first changes on the original template

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+// Function linked to the hamburg menu (when screen is < 1200px)
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
+  menu.classList.toggle("open"); //toggle function with open class linked 
   icon.classList.toggle("open");
 }
